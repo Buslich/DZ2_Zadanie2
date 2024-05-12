711bb5e68495756905dc71d689fae89a96554089
Buslich <Buslich@yandex.ru>
2024-05-12 12:16:37 +0400

    Изменение2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
         // переменные для начального счета
         int initialBalance = 50; // изначальный баланс пользователя
-        int replenishment = 200; // сумма, которую он пополняет
+        int replenishment = 4000; // сумма, которую он пополняет
 
         // переменные для бонуса и итогового счета
         int bonus = 0;
         int finalBalance = initialBalance + replenishment;
 
         // проверяем, превышает ли сумма порог в 1000
-        if (initialBalance > 1000) {
+        if (finalBalance > 1000) {
             bonus = replenishment / 100;
         }
 
